053505c78de2079a79da8354b15e2b9b0f5977ac
Paindar <1944798085@qq.com>
2018-03-19 01:17:22 +0800

    Move FileIndex as alone file

--- DataManager.cs
+++ DataManager.cs
@@ -2,52 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace FileManagerProject_ConsoleVersion
 {
     class DataManager
     {
-        class FileIndex
-        {
-            String path;
-            String MD5;
-            long size;
-            DateTime time;
-            //TODO may need List<tag> to generate invert table
-            public FileIndex(string s)
-            {
-                string[] args = s.Split('\b');
-                if (args.Length != 4)
-                    throw new Exception("Incorrect parameter.");
-                path = args[0];
-                MD5 = args[1];
-                size = Convert.ToInt64(args[2]);
-                time = Convert.ToDateTime(args[3]);
-            }
-            public FileIndex(ref FileInfo info)
-            {
-                this.path = info.FullName;
-                this.MD5 = Utils.GetMD5(path);
-                this.size = info.Length;
-                this.time = info.LastWriteTime;
-            }
-            public string ConvertToString()
-            {
-                StringBuilder builder = new StringBuilder();
-                builder.Append(path);
-                builder.Append('\b');
-                builder.Append(MD5);
-                builder.Append('\b');
-                builder.Append(size);
-                builder.Append('\b');
-                builder.Append(time.ToString("o"));
-                return builder.ToString();
-            }
-            public string GetFullPath() => path;
-            public long GetSize() => size;
-            public DateTime GetTime() => this.time;
-        }
+        
         List<FileIndex> fileInfos = new List<FileIndex>();
         Hashtable table = new Hashtable();
 
---
+++ FileIndex.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagerProject_ConsoleVersion
+{
+    class FileIndex
+    {
+        String path;
+        String MD5;
+        long size;
+        DateTime time;
+        //TODO may need List<tag> to generate invert table
+        public FileIndex(string s)
+        {
+            string[] args = s.Split('\b');
+            if (args.Length < 4)
+                throw new Exception("Incorrect parameter.");
+            path = args[0];
+            MD5 = args[1];
+            size = Convert.ToInt64(args[2]);
+            time = Convert.ToDateTime(args[3]);
+        }
+        public FileIndex(ref System.IO.FileInfo info)
+        {
+            this.path = info.FullName;
+            this.MD5 = Utils.GetMD5(path);
+            this.size = info.Length;
+            this.time = info.LastWriteTime;
+        }
+        public void AddTags(params string[] tags)
+        {
+            foreach (string tag in tags)
+                this.tags.Add(tag);
+        }
+        public string ConvertToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(path);
+            builder.Append('\b');
+            builder.Append(MD5);
+            builder.Append('\b');
+            builder.Append(size);
+            builder.Append('\b');
+            builder.Append(time.ToString("o"));
+            builder.Append('\b');
+            return builder.ToString();
+        }
+        public string GetFullPath() => path;
+        public long GetSize() => size;
+        public DateTime GetTime() => this.time;
+    }
+}
